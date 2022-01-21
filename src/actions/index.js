6caebe812bf791e962d3ddb51974a98a0274855e
bezzantsam <83626945+bezzantsam@users.noreply.github.com>
2022-01-20 20:02:08 -0800

    logic

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
+
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAILURE = "FETCH_FAILURE";
+export const ADD_SMURF = "ADD_SMURF";
+export const ADD_ERROR = "ADD_ERROR";
+
+export const fetchSmurfs = () => (dispatch) => {
+  dispatch({ type: FETCH_START });
+  axios
+    .get("http://localhost:3333/smurfs")
+    .then((res) => dispatch({ type: FETCH_SUCCESS, payload: res.data }))
+    .catch((err) => dispatch({ type: FETCH_FAILURE, payload: err.message }));
+};
+export const addSmurf = ({ id, name, position, nickname, description }) => {
+  return {
+    type: ADD_SMURF,
+    payload: { id, name, position, nickname, description },
+  };
+};
+
+export const addError = (err) => {
+  return { type: ADD_ERROR, payload: err };
+};
 
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
